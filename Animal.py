bae7ce28fe01be46067f7b9d16efe644065cdeaf
SUCHITRA7679 <koresuchitra2005@gmail.com>
2025-07-03 12:19:25 +0530

    Delete Animal.py

--- Animal.py
+++
@@ -1,9 +0,0 @@
-class Animal:
-    def speak(self):
-        print("Animal speaks")
-class Dog(Animal):
-    def bark(self):
-        print("dog bargs")
-d=Dog()
-d.speak()
-d.bark()                
